2acee32a287a3b6f089b145ef2153350630fac58
Surkov Pavel <ps@studiotribe.ru>
2024-06-24 18:06:26 +0700

    feat: make physics more realistic

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -14,7 +14,16 @@ import { MeshLineGeometry, MeshLineMaterial } from 'meshline';
 
 extend({ MeshLineGeometry, MeshLineMaterial });
 
-export default function Badge() {
+// Make RigidBody physics a bit more realistic
+const segmentProps = {
+  type: 'dynamic',
+  canSleep: true,
+  colliders: false,
+  angularDamping: 2,
+  linearDamping: 2,
+} as const;
+
+export default function Badge({ maxSpeed = 50, minSpeed = 10 }) {
   // References for the band and the joints
   const band = useRef<THREE.Mesh<MeshLineGeometry, MeshLineMaterial>>(null);
   const fixed = useRef<RapierRigidBody>(null);
@@ -52,7 +61,7 @@ export default function Badge() {
     [0, 1.45, 0],
   ]);
 
-  useFrame((state) => {
+  useFrame((state, delta) => {
     if (
       !fixed.current ||
       !j1.current ||
@@ -67,6 +76,7 @@ export default function Badge() {
       vec.set(state.pointer.x, state.pointer.y, 0.5).unproject(state.camera);
       dir.copy(vec).sub(state.camera.position).normalize();
       vec.add(dir.multiplyScalar(state.camera.position.length()));
+      [card, j1, j2, j3, fixed].forEach((ref) => ref.current?.wakeUp());
       card.current.setNextKinematicTranslation({
         x: vec.x - dragged.x,
         y: vec.y - dragged.y,
@@ -74,12 +84,28 @@ export default function Badge() {
       });
     }
 
+    // Fix most of the band jitter when over pulling the card (https://codesandbox.io/s/sweet-galois-3fw3wq?file=/App.js:3796-3849)
+    const [j1Lerped, j2Lerped] = [j1, j2].map((ref) => {
+      if (ref.current) {
+        const lerped = new THREE.Vector3().copy(ref.current.translation());
+
+        const clampedDistance = Math.max(
+          0.1,
+          Math.min(1, lerped.distanceTo(ref.current.translation()))
+        );
+
+        return lerped.lerp(
+          ref.current.translation(),
+          delta * (minSpeed + clampedDistance * (maxSpeed - minSpeed))
+        );
+      }
+    });
+
     // Calculate Catmull curve for band
     curve.points[0].copy(j3.current.translation());
-    curve.points[1].copy(j2.current.translation());
-    curve.points[2].copy(j1.current.translation());
+    curve.points[1].copy(j2Lerped ?? j2.current.translation());
+    curve.points[2].copy(j1Lerped ?? j1.current.translation());
     curve.points[3].copy(fixed.current.translation());
-
     band.current.geometry.setPoints(curve.getPoints(32));
 
     // Tilt the card back towards the screen
@@ -91,39 +117,50 @@ export default function Badge() {
     );
   });
 
+  curve.curveType = 'chordal';
+
   return (
     <>
-      <RigidBody ref={fixed} type="fixed" position={[0, 3, 0]} />
-      <RigidBody position={[0.5, 3, 0]} ref={j1}>
-        <BallCollider args={[0.1]} />
-      </RigidBody>
-      <RigidBody position={[1, 3, 0]} ref={j2}>
-        <BallCollider args={[0.1]} />
-      </RigidBody>
-      <RigidBody position={[1.5, 3, 0]} ref={j3}>
-        <BallCollider args={[0.1]} />
-      </RigidBody>
+      <group position={[0, 3, 0]}>
+        {/* Band */}
+        <RigidBody ref={fixed} type="fixed" position={[0, 0, 0]} />
+        <RigidBody position={[0.5, 0, 0]} {...segmentProps} ref={j1}>
+          <BallCollider args={[0.1]} />
+        </RigidBody>
+        <RigidBody position={[1, 0, 0]} {...segmentProps} ref={j2}>
+          <BallCollider args={[0.1]} />
+        </RigidBody>
+        <RigidBody position={[1.5, 0, 0]} {...segmentProps} ref={j3}>
+          <BallCollider args={[0.1]} />
+        </RigidBody>
+
+        {/* Card */}
+        <RigidBody
+          ref={card}
+          {...segmentProps}
+          type={dragged ? 'kinematicPosition' : 'dynamic'}
+        >
+          <CuboidCollider args={[0.8, 1.125, 0.01]} />
+          <mesh
+            onPointerUp={() => drag(false)}
+            onPointerDown={(evt) =>
+              card.current &&
+              drag(
+                new THREE.Vector3()
+                  .copy(evt.point)
+                  .sub(vec.copy(card.current.translation()))
+              )
+            }
+          >
+            <planeGeometry args={[0.8 * 2, 1.125 * 2]} />
+            <meshBasicMaterial color="white" side={THREE.DoubleSide} />
+          </mesh>
+        </RigidBody>
+      </group>
       <mesh ref={band}>
         <meshLineGeometry />
-        <meshLineMaterial color="black" lineWidth={0.2} />
+        <meshLineMaterial color="white" lineWidth={0.2} />
       </mesh>
-      <RigidBody ref={card} type={dragged ? 'kinematicPosition' : 'dynamic'}>
-        <CuboidCollider args={[0.8, 1.125, 0.01]} />
-        <mesh
-          onPointerUp={() => drag(false)}
-          onPointerDown={(evt) =>
-            card.current &&
-            drag(
-              new THREE.Vector3()
-                .copy(evt.point)
-                .sub(vec.copy(card.current.translation()))
-            )
-          }
-        >
-          <planeGeometry args={[0.8 * 2, 1.125 * 2]} />
-          <meshBasicMaterial color="black" side={THREE.DoubleSide} />
-        </mesh>
-      </RigidBody>
     </>
   );
 }
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,13 +1,54 @@
 import { Canvas } from '@react-three/fiber';
 import { Physics } from '@react-three/rapier';
 import Badge from './Badge';
+import { Environment } from '@react-three/drei';
+import { useControls } from 'leva';
 
 export default function CanvasComponent() {
+  const { debug } = useControls({ debug: true });
+
   return (
     <Canvas>
-      <Physics debug>
+      <ambientLight intensity={Math.PI} />
+      <Physics
+        debug={debug}
+        interpolate
+        gravity={[0, -38, 0]}
+        timeStep={1 / 60}
+      >
         <Badge />
       </Physics>
+      <Environment background blur={0.75}>
+        <color attach="background" args={['black']} />
+        {/* <Lightformer
+          intensity={2}
+          color="white"
+          position={[0, -1, 5]}
+          rotation={[0, 0, Math.PI / 3]}
+          scale={[100, 0.1, 1]}
+        />
+        <Lightformer
+          intensity={3}
+          color="white"
+          position={[-1, -1, 1]}
+          rotation={[0, 0, Math.PI / 3]}
+          scale={[100, 0.1, 1]}
+        />
+        <Lightformer
+          intensity={3}
+          color="white"
+          position={[1, 1, 1]}
+          rotation={[0, 0, Math.PI / 3]}
+          scale={[100, 0.1, 1]}
+        />
+        <Lightformer
+          intensity={10}
+          color="white"
+          position={[-10, 0, 14]}
+          rotation={[0, Math.PI / 2, Math.PI / 3]}
+          scale={[100, 10, 1]}
+        /> */}
+      </Environment>
     </Canvas>
   );
 }
